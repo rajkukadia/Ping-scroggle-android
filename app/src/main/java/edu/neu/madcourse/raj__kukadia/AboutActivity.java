a681590be2289ecf98ebc60a421304639c08a415
Raj Kukadia <rajkukadia1@gmail.com>
2017-01-20 20:25:06 -0500

    bugs fixed

--- app/src/main/java/edu/neu/madcourse/raj__kukadia/AboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/raj__kukadia/AboutActivity.java
@@ -25,7 +25,7 @@ import android.view.View;
 public class AboutActivity extends Activity implements View.OnClickListener {
 
     private AlertDialog mDialog;
-    private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+    //private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
     private static TelephonyManager mngr;
     private static String x;
     private AlertDialog.Builder builder;
@@ -44,7 +44,7 @@ public class AboutActivity extends Activity implements View.OnClickListener {
             public void onClick(View v) {
                 // int permissionCheck = ContextCompat.checkSelfPermission(AboutActivity.this,
                 //   Manifest.permission.READ_PHONE_STATE);
-                checkPermissions();
+
 
                 //  try {
                 //    Thread.sleep(1500);
@@ -66,6 +66,7 @@ public class AboutActivity extends Activity implements View.OnClickListener {
 
                         });
                 mDialog = builder.show();
+                Log.d("error","s");
 
             }
 
@@ -76,63 +77,11 @@ public class AboutActivity extends Activity implements View.OnClickListener {
 
     }
 
-    protected void checkPermissions() {
+   // protected void checkPermissions() {
 
-    // Here, thisActivity is the current activity
-    if (ContextCompat.checkSelfPermission(AboutActivity.this,
-            Manifest.permission.READ_PHONE_STATE)
-            != PackageManager.PERMISSION_GRANTED)
-
-    {
-
-        // Should we show an explanation?
-        if (ActivityCompat.shouldShowRequestPermissionRationale(AboutActivity.this,
-                Manifest.permission.READ_PHONE_STATE)) {
-            // Show an explanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
-        } else {
-
-            // No explanation needed, we can request the permission.
-            ActivityCompat.requestPermissions(AboutActivity.this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-        }
-
-    }
+//}
 
 
-}
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
 
 
     @Override
--- app/src/main/java/edu/neu/madcourse/raj__kukadia/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/raj__kukadia/MainActivity.java
@@ -1,10 +1,14 @@
 package edu.neu.madcourse.raj__kukadia;
 
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
 import java.util.HashMap;
@@ -13,7 +17,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 
     private static HashMap<Integer, View> viewMap = new HashMap<Integer, View>();
-
+    private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
     public MainActivity() {
 
     }
@@ -22,12 +26,71 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        checkPermissions();
         getActionBar().setTitle("Raj Kukadia");
         setContentView(R.layout.activity_main);
         //new MainActivity();
         setHashMap();
         setListner(viewMap);
+
+
+
     }
+
+private void checkPermissions() {
+    // Here, thisActivity is the current activity
+    if (ContextCompat.checkSelfPermission(MainActivity.this,
+            Manifest.permission.READ_PHONE_STATE)
+            != PackageManager.PERMISSION_GRANTED)
+
+    {
+
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE)) {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+        } else {
+
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+
+    }
+
+}
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 
     protected void setHashMap() {
         View v = findViewById(R.id.about_mainactivity_button);
