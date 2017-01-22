3656fa606f36d4f5cac85721b39dc7f127349f00
Raj Kukadia <rajkukadia1@gmail.com>
2017-01-21 20:59:43 -0500

    new commit

--- app/src/main/java/edu/neu/madcourse/raj__kukadia/AboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/raj__kukadia/AboutActivity.java
@@ -1,35 +1,19 @@
-/***
- * Excerpted from "Hello, Android",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/eband4 for more book information.
- ***/
 package edu.neu.madcourse.raj__kukadia;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
-import android.util.AndroidRuntimeException;
-import android.util.Log;
 import android.view.View;
 
 public class AboutActivity extends Activity implements View.OnClickListener {
 
     private AlertDialog mDialog;
-    //private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
     private static TelephonyManager mngr;
     private static String x;
     private AlertDialog.Builder builder;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,20 +26,9 @@ public class AboutActivity extends Activity implements View.OnClickListener {
         v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // int permissionCheck = ContextCompat.checkSelfPermission(AboutActivity.this,
-                //   Manifest.permission.READ_PHONE_STATE);
-
-
-                //  try {
-                //    Thread.sleep(1500);
-                //} catch (InterruptedException e) {
-                //  e.printStackTrace();
-                //}
-
-
 
                 mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-                builder.setMessage( mngr.getDeviceId());
+                builder.setMessage(mngr.getDeviceId());
                 builder.setCancelable(false);
                 builder.setPositiveButton(R.string.ok_label,
                         new DialogInterface.OnClickListener() {
@@ -66,23 +39,12 @@ public class AboutActivity extends Activity implements View.OnClickListener {
 
                         });
                 mDialog = builder.show();
-                Log.d("error","s");
 
             }
 
 
         });
-
-
-
     }
-
-   // protected void checkPermissions() {
-
-//}
-
-
-
 
     @Override
     protected void onResume() {
--- app/src/main/java/edu/neu/madcourse/raj__kukadia/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/raj__kukadia/MainActivity.java
@@ -1,8 +1,6 @@
 package edu.neu.madcourse.raj__kukadia;
 
-
 import android.Manifest;
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -18,6 +16,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static HashMap<Integer, View> viewMap = new HashMap<Integer, View>();
     private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+
     public MainActivity() {
 
     }
@@ -29,43 +28,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
         checkPermissions();
         getActionBar().setTitle("Raj Kukadia");
         setContentView(R.layout.activity_main);
-        //new MainActivity();
         setHashMap();
         setListner(viewMap);
 
-
-
     }
 
-private void checkPermissions() {
-    // Here, thisActivity is the current activity
-    if (ContextCompat.checkSelfPermission(MainActivity.this,
-            Manifest.permission.READ_PHONE_STATE)
-            != PackageManager.PERMISSION_GRANTED)
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
 
-    {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+                //nothing
 
-        // Should we show an explanation?
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                Manifest.permission.READ_PHONE_STATE)) {
-            // Show an explanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
-        } else {
+            } else {
 
-            // No explanation needed, we can request the permission.
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
 
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+            }
+
         }
 
     }
 
-}
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
@@ -74,20 +63,12 @@ private void checkPermissions() {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
 
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                 }
-                return;
+
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
