3deb03ec8f0c08492fb1d0666517e7722798ed9b
Raj Kukadia <rajkukadia1@gmail.com>
2017-04-12 19:55:18 -0400

    Ping - second update

--- app/src/main/java/edu/neu/madcourse/raj__kukadia/ping/MyContactsActivity.java
+++ app/src/main/java/edu/neu/madcourse/raj__kukadia/ping/MyContactsActivity.java
@@ -4,16 +4,20 @@ package edu.neu.madcourse.raj__kukadia.ping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import edu.neu.madcourse.raj__kukadia.R;
 
 public class MyContactsActivity extends Activity implements View.OnClickListener {
 
-
+    private ListView myContacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_contacts_ping);
+
+        myContacts = (ListView) findViewById(R.id.contact_list);
+
 
     }
 
