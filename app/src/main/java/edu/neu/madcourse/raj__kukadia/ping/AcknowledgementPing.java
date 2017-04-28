afd025d7af652b999bfa9b594d8961b30da5eff5
dharaksavalia <dharak.savalia@gmail.com>
2017-04-28 12:58:43 -0400

    Ping-Acknowledgment added Code still not working

---
+++ app/src/main/java/edu/neu/madcourse/raj__kukadia/ping/AcknowledgementPing.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.raj__kukadia.ping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.neu.madcourse.raj__kukadia.R;
+
+public class AcknowledgementPing extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acknowledgement_ping);
+
+    }
+}
